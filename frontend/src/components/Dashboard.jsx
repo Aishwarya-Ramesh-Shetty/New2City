c73be0ec119a5faf6f2da3d335629be9e1fc7d16
Aishu-bit <shettyaishu050@gmail.com>
2025-09-17 19:19:22 +0530

    Create ExploreCategory Component

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import ExploreCategory from './ExploreCategory'
 
 const Dashboard = () => {
   return (
     <>
-        <div >
+        <div className='flex flex-col'>
             <nav><Navbar/></nav>
-
+            <div><ExploreCategory/></div>
         </div>
         
     </>
