f123a4952f363df173cb113a8a64463301dd4896
rajwaghamare <115868590+rajwaghamare@users.noreply.github.com>
2025-09-27 18:14:09 +0530

    Update ExploreCategory.jsx

--- frontend/src/components/ExploreCategory.jsx
+++ frontend/src/components/ExploreCategory.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const ExploreCategory = () => {
   return (
     <div className='flex flex-col justify-center items-center mt-[100px]'>
+        <h1>Welcome to NEW2CITY</h1>
         <p className=' text-3xl font-bold'>Explore by Category</p>
         <p className='mt-4 w-[660px] text-center text-xl text-[#889696] '>Whether you're looking for great food, cultural experiences, or places to relax, we've got you covered with curated recommendations.</p>
     </div>
