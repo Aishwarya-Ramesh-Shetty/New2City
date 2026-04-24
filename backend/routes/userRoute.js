c938dd9cc9143506a34e14c7f894f82f63a1bcfe
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-04-24 23:27:49 +0530

    Create Auth middleware for Authorization

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,4 +5,8 @@ const {userRegister,userLogin} = require('../controllers/userController')
 router.post('/register',userRegister);
 router.post('/login',userLogin);
 
+router.get('/profile',protect,(req,res)=>{
+    res.json(req.user);
+})
+
 module.exports = router;
